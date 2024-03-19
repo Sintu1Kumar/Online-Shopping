24ea562a79ab69389d58551916394fac2c609a08
Sintu Kumar <kumarsintu499@gmail.com>
2024-03-19 21:21:59 +0530

    setup contact form data

--- src/pages/Contact-Us/ContactUs.js
+++ src/pages/Contact-Us/ContactUs.js
@@ -1,59 +1,58 @@
-import React, { useState } from 'react';
-import { Container, Form, Button, Row, Col } from 'react-bootstrap';
-import axios from 'axios';
-import contactusImage from '../../assets/contact-us.jpg';
-import './ContactUs.css';
+import React, { useState } from "react";
+import { Container, Form, Button, Row, Col } from "react-bootstrap";
+import axios from "axios";
+import contactusImage from "../../assets/contact-us.jpg";
+import "./ContactUs.css";
 
 const ContactPage = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
 
   const nameHandler = (e) => {
     setName(e.target.value);
-  }
+  };
 
   const emailHandler = (e) => {
     setEmail(e.target.value);
-  }
+  };
 
   const phoneHandler = (e) => {
     setPhone(e.target.value);
-  }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = {
-        name: name,
-        email: email,
-        phone: phone
-    }
+      name: name,
+      email: email,
+      phone: phone,
+    };
     try {
-        const response = await axios.post('https://e-commerce-1db2b-default-rtdb.firebaseio.com/contact.json', formData);
+      const response = await axios.post(
+        "https://online-shoping-2cfcb-default-rtdb.firebaseio.com/contact.json",
+        formData
+      );
 
-        if(response.status !== 200){
-            throw new Error('Something went wrong...');
-        }
-
-        console.log('Form submitted successfully:', response);
-        
-        setName('');
-        setEmail('');
-        setPhone('');
-      } catch (error) {
-        console.error(error.message);
+      if (response.status !== 200) {
+        throw new Error("Something went wrong...");
       }
+
+      alert("Form submitted successfully:", response);
+
+      setName("");
+      setEmail("");
+      setPhone("");
+    } catch (error) {
+      console.error(error.message);
+    }
   };
 
   return (
     <Container className="contact-form mb-4">
-     <Row className='align-items-center justify-content-between'>
+      <Row className="align-items-center justify-content-between">
         <Col md={7} lg={6} xl={6}>
-          <img
-            src={contactusImage}
-            className="img-fluid"
-            alt="Contact-Us"
-          />
+          <img src={contactusImage} className="img-fluid" alt="Contact-Us" />
         </Col>
         <Col md={4} lg={5} xl={5} className="shadow p-4">
           <Form onSubmit={handleSubmit}>
